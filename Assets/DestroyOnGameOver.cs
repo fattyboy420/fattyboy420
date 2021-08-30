55cc13a93a980ff26937e718bf846b8d75613239
fattyboy420 <gervyramon@yahoo.com>
2021-08-30 18:49:03 +0800

    ww
    Here we do this

---
+++ Assets/DestroyOnGameOver.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DestroyOnGameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Scene scene = SceneManager.GetActiveScene();
+if (scene.name=="GameOver"){
+			Destroy(gameObject);
+			Debug.Log("Audio Destroyed");
+			}
+			
+			
+    }
+}
---
+++ Assets/MazeCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class MazeCounter : MonoBehaviour
+{
+   public Text Score_UIText; // assign it from inspector
+   
+    
+ void Start()
+ {
+		
+	 
+    Score_UIText.text ="Maze: " + LevelGenerator.mazeLevel;
+ }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
